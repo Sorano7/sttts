5a480cedbbdd5446c85df529c5d6d6e940540533
Sorano7 <sorano27@outlook.com>
2025-05-05 16:55:59 -0400

    Add pipeline runner to start process

--- app/config.py
+++ app/config.py
@@ -1,9 +1,31 @@
+import yaml
+from dataclasses import dataclass, asdict
+from typing import List
+from lingua import Language
+
+@dataclass
 class Config:
-  def __init__(
-    self,
-    output_device: str,
-    stt_mode = 'small',
-    enable_tts = True,
-    enable_osc = True,
-  ):
-    pass
+  output_device: str
+  stt_model: str = 'small'
+  enable_tts: bool = True
+  enable_osc: bool = True
+  language_to_detect: List[str] = ["ENGLISH", "JAPANESE", "CHINESE"]
+  
+  def validate(self):
+    for lang in self.language_to_detect:
+      if not Language.from_str(lang):
+        raise ValueError('Invalid language')
+  
+  @classmethod
+  def load(cls, path: str):
+    with open(path, 'r') as f:
+      data = yaml.safe_load(f)
+      
+    obj = cls(**data)
+    obj.validate()
+    return obj
+    
+  
+  def save(self, path: str):
+    with open(path, 'w') as f:
+      yaml.safe_dump(asdict(self), f)
--- app/pipeline/runner.py
+++ app/pipeline/runner.py
@@ -12,9 +12,28 @@ class Runner:
   ):
     self.config = config
     
-  def load_config(self):
-    pass
-  
-  async def start(self):
-    pass
-
+    self.audio_queue = asyncio.Queue()
+    self.text_queue = asyncio.Queue()
+    
+    self.audio_recorder = AudioRecorder(self.audio_queue)
+    self.transcriber = WhisperClient(self.audio_queue, self.text_queue, self.config.stt_model)
+    self.text_processor = TextProcessor(self.text_queue, self.config.enable_tts, self.config.enable_osc)
+    
+  async def run(self):
+    record_task = asyncio.create_task(self.audio_recorder.start())
+    transcribe_task = asyncio.create_task(self.transcriber.start())
+    text_task = asyncio.create_task(self.text_processor.start())
+    
+    try:
+      while True:
+        await asyncio.sleep(0.1)
+    finally:
+      await self.audio_recorder.stop()
+      await self.transcriber.stop()
+      await self.text_processor.stop()
+      
+      await asyncio.gather(
+        record_task,
+        transcribe_task,
+        text_task,
+      )
