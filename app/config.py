e7f38671ba436e5adff6f283aa189e1288d985a7
Sorano7 <sorano27@outlook.com>
2025-05-09 15:34:01 -0400

    Move config loading logic to Config class

--- app/config.py
+++ app/config.py
@@ -1,7 +1,9 @@
 import yaml
+import os
 from dataclasses import dataclass, asdict, field
 from typing import List, Dict
 from lingua import Language
+from .audio.utils import get_default_output_name
 
 @dataclass
 class Config:
@@ -11,9 +13,9 @@ class Config:
   enable_osc: bool = True
   tts_models: Dict[str, str] = field(
     default_factory=lambda: {
-      "ENGLISH": "en-US-AnaNeural",
+      "ENGLISH": "en-US-AriaNeural",
       "JAPANESE": "ja-JP-NanamiNeural",
-      "CHINESE": "zh-CN-XiaoyiNeural"
+      "CHINESE": "zh-CN-XiaoxiaoNeural"
     }
   )
 
@@ -27,13 +29,31 @@ class Config:
   
   @classmethod
   def load(cls, path: str):
-    with open(path, 'r') as f:
-      data = yaml.safe_load(f)
+    try:
+      with open(path, 'r') as f:
+        data = yaml.safe_load(f)
+        
+      obj = cls(**data)
+      obj.validate()
+      return obj
+    except Exception as e:
+      print(f"[Config] Invalid config file: {e}. Generating new config file...")
+      return cls.new(path)
       
-    obj = cls(**data)
-    obj.validate()
+  @classmethod
+  def new(cls, path: str):
+    try:
+      old_config_path = f"{path}.old"
+      os.rename(path, old_config_path)
+    except FileNotFoundError:
+      pass
+    except Exception as e:
+      print(f"[Config] Unable to rename old config file. Overwriting...")
+      
+    obj = cls(get_default_output_name())
+    obj.save(path)
     return obj
-    
+  
   def save(self, path: str):
     with open(path, 'w') as f:
       yaml.safe_dump(asdict(self), f)
--- app/main.py
+++ app/main.py
@@ -1,43 +1,21 @@
 import os
+import sys
 import asyncio
 from .pipeline.runner import Runner
 from .config import Config
-from .audio.utils import get_default_output_name
+
 
 class STTTS:
-  CONFIG_PATH = "./config.yaml"
-  
   def __init__(self):
-    self.load_config()
+    self.config_path = self._get_config_path()
+    self.config = Config.load(self.config_path)
     self.runner = Runner(self.config)
   
-  def load_config(self):
-    try:
-      config = Config.load(self.CONFIG_PATH)
-      self.config = config
-    except Exception as e:
-      print(f"[STTTS] Invalid config file: {e}. Generating new config file...")
-      try:
-        os.rename(self.CONFIG_PATH, f"{self.CONFIG_PATH}.old")
-      except:
-        pass
-      self._generate_new_config()
-      
-  def _generate_new_config(self):
-    try:
-      old_config_path = f"{self.CONFIG_PATH}.old"
-      os.rename(self.CONFIG_PATH, old_config_path)
-    except FileNotFoundError:
-      pass
-    except Exception as e:
-      print("[STTTS] Failed to rename old config file. Removing...")
-    device = get_default_output_name()
-    self.config = Config(output_device=device)
-    self.config.save(self.CONFIG_PATH)
-  
-  def run_gui(self):
-    pass
-    
+  def _get_config_path(self):
+    exe_path = sys.executable if getattr(sys, 'frozen', False) else __file__
+    exe_dir = os.path.dirname(os.path.abspath(exe_path))
+    return os.path.join(exe_dir, "config.yml")
+
   def run(self):
     print("[STTTS] Starting...")
     try:
@@ -45,7 +23,9 @@ class STTTS:
     except KeyboardInterrupt:
       print("[STTTS] Exited.")
   
-  
-if __name__ == "__main__":
+def main():
   sttts = STTTS()
   sttts.run()
+  
+if __name__ == "__main__":
+  main()
