12af4693c66b77effb1faab2282243ba9f217360
Sorano7 <sorano27@outlook.com>
2025-05-09 23:46:08 -0400

    Add error handling

--- app/text/vrchat/osc_client.py
+++ app/text/vrchat/osc_client.py
@@ -1,8 +1,14 @@
 from pythonosc.udp_client import SimpleUDPClient
+from ...logger import get_logger
+
+logger = get_logger("OSC")
 
 class OSCClient:
   def __init__(self):
     self.client = SimpleUDPClient("127.0.0.1", 9000)
     
   def process_text(self, text):
-    self.client.send_message("/chatbox/input", [text, True])
+    try:
+      self.client.send_message("/chatbox/input", [text, True])
+    except Exception as e:
+      logger.error(f"{e}. Is VRChat running?")
