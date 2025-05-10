adce74185f337c7b129c4fd1a4c6beb54d0cb7f3
Sorano7 <sorano27@outlook.com>
2025-05-09 22:29:48 -0400

    Update logger version

--- app/logger.py
+++ app/logger.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from enum import Enum
+from pathlib import Path
 from inspect import currentframe
+import re
 
 class LogLevel(Enum):
   DEBUG = 10
@@ -17,13 +19,34 @@ class LogRunner:
   """Wrapper class to handle logging and global settings"""
   
   def __init__(self, level):
-    self.level = _get_level(level)
+    self.set_level(level)
+    self.format = "[time(%H:%M)] [level] [file] [name] msg"
   
   def should_print(self, level):
     return _get_level(level) >= self.level
   
   def set_level(self, level):
     self.level = _get_level(level)
+    
+  def set_format(self, format: str):
+    self.format = format
+      
+  def format_log(self, level, msg, time, name, file, line):
+    def replace(match):
+      token = match.group(1)
+      if token.startswith('time(') and token.endswith(')'):
+        fmt = token[5:-1]
+        return time.strftime(fmt)
+      elif token == 'level':
+        return level
+      elif token == 'file':
+        return f"{file}:{line}"
+      elif token == 'name':
+        return name
+      elif token == 'msg':
+        return msg
+      
+    return re.sub(r'(time\([^\)]+\)|level|file|name|msg)', replace, self.format)
 
 class Logger:
   """Class for logging messages"""
@@ -34,8 +57,12 @@ class Logger:
     self.level = None if level is None else _get_level(level)
     
   def _log(self, level, msg):
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    output = f"[{timestamp}] [{level}] [{self.name}] {msg}"
+    timestamp = datetime.now()
+    frame = currentframe().f_back.f_back
+    filename = frame.f_code.co_filename
+    filename = Path(filename).name
+    lineno = frame.f_lineno
+    output = self.runner.format_log(level, msg, timestamp, self.name, filename, lineno)
     
     if self.level is not None and _get_level(level) >= self.level:
       print(output)
@@ -58,19 +85,23 @@ class Logger:
   def critical(self, msg):
     self._log("CRITICAL", msg)
     
-runner = LogRunner("INFO")
+runner = LogRunner("DEBUG")
 
 def set_global_log_level(level):
   """Set the global log level"""
   
   runner.set_level(level)
+  
+def set_global_log_format(format: str):
+  """Set the global log format.
+  
+  Examples:
+    - [time(%Y-%m-%d %H:%M)] [level] [file] msg
+    - time(%H:%M:%S) | level | name: "msg"
+  """
+  runner.set_format(format)
     
 def get_logger(name="Main", level=None):
   """Get a logger object under the global LogRunner"""
   
   return Logger(runner, name, level)
-    
-def get_linenumber():
-  cf = currentframe()
-  return cf.f_back.f_lineno
-  
